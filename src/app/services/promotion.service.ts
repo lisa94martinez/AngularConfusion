2543d6eb6386484645fcd979c04e5be154ba5231
Lisa <lisa_martinez@dusty.tamiu.edu>
2017-09-21 17:50:08 -0500

    Promise Part 2

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -7,15 +7,25 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promise<Promotion[]> {
-    return Promise.resolve(PROMOTIONS);
-  }
 
-  getPromotion(id: number): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]);
-  }
+    getPromotions(): Promise<Promotion[]> {
+      return new Promise(resolve=> {
+        // Simulate server latency with 2 second delay
+          setTimeout(() => resolve(PROMOTIONS), 2000);
+      });
+    }
 
-  getFeaturedPromotion(): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => promo.featured)[0]);
-  }
+    getPromotion(id: number): Promise<Promotion> {
+      return new Promise(resolve=> {
+        // Simulate server latency with 2 second delay
+          setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
+      });
+    }
+
+    getFeaturedPromotion(): Promise<Promotion> {
+      return  new Promise(resolve=> {
+        // Simulate server latency with 2 second delay
+          setTimeout(() => resolve(PROMOTIONS.filter((promo) => promo.featured)[0]), 2000);
+      });
+    }
 }
