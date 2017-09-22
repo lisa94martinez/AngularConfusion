90286d0ba2a76f565a8b9a196f006391a0a7bea6
Lisa <lisa_martinez@dusty.tamiu.edu>
2017-09-22 18:44:35 -0500

    REST Save

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -17,6 +17,7 @@ import 'rxjs/add/operator/switchMap';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishcopy =null;
   dishIds: number[];
   prev: number;
   next: number;
@@ -50,7 +51,7 @@ export class DishdetailComponent implements OnInit {
       this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
       this.route.params
         .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+        .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
         errmess => this.errMess = <any>errmess);
     }
 
@@ -96,7 +97,9 @@ export class DishdetailComponent implements OnInit {
   onSubmit() {
     this.comment = this.feedbackForm.value;
     this.comment.date = new Date().toISOString();
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishcopy.save()
+      .subscribe(dish =>this.dish = dish);
     this.feedbackForm.reset({
       author: '',
       comment: '',
