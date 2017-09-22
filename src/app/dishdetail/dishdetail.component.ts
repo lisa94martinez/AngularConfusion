ee750122329c295aea2b832ac60ebd70bb853ec2
Lisa <lisa_martinez@dusty.tamiu.edu>
2017-09-22 17:39:58 -0500

    HTTP Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -26,6 +26,7 @@ export class DishdetailComponent implements OnInit {
     'author': '',
     'comment': ''
   };
+    errMess: string;
 
   validationMessages = {
     'author': {
@@ -49,7 +50,8 @@ export class DishdetailComponent implements OnInit {
       this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
       this.route.params
         .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+        errmess => this.errMess = <any>errmess);
     }
 
     setPrevNext(dishId: number) {
