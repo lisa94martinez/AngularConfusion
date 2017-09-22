9aa2b9f2f169398a1036decf2bce8a61d63f6e1d
Lisa <lisa_martinez@dusty.tamiu.edu>
2017-09-22 17:09:43 -0500

    HTTP Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import 'hammerjs';
 import { AppComponent } from './app.component';
@@ -21,7 +22,9 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { baseURL } from './shared/baseurl';
+import { ProcessHttpmsgService } from './services/process-httpmsg.service'
 
 @NgModule({
   declarations: [
@@ -42,11 +45,14 @@ import { LoginComponent } from './login/login.component'
     FlexLayoutModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+     HttpModule
   ],
   providers: [DishService,
   PromotionService,
-LeaderService],
+  LeaderService,
+  {provide: 'BaseURL', useValue: baseURL},
+  ProcessHttpmsgService],
 
   entryComponents: [
        LoginComponent
