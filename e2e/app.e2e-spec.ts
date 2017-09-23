1a96b27c653d10cc9193209801cd27143ea376da
Lisa <lisa_martinez@dusty.tamiu.edu>
2017-09-22 22:49:54 -0500

    tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,24 @@
-import { AppPage } from './app.po';
+import { ConFusionPage } from './app.po';
 
 describe('confusion App', () => {
-  let page: AppPage;
+  let page: ConFusionPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new ConFusionPage();
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
+
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    let navlink = page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us')
+  });
+
 });
